c7adc55d748c27fa81c8907ad6cf236a937fcd6f
washingtonekinzi <washingtonekinzi@gmail.com>
2025-09-18 17:41:31 -0700

    added

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,3 +1,4 @@
 console.log('7-multi_c.js 2 C is fun C is fun')
 console.log('7-multi_c.js 5 C is fun C is fun C is fun C is fun C is fun')
 console.log('7-multi_c.js Missing number of occurrences')
+console.log('7-multi_c.js -3')
