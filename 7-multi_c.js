c8450eb01b25032abc32211d2495e1c48faac657
washingtonekinzi <washingtonekinzi@gmail.com>
2025-09-21 12:51:39 -0700

    7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,4 +1,1 @@
-console.log('7-multi_c.js 2 C is fun C is fun')
-console.log('7-multi_c.js 5 C is fun C is fun C is fun C is fun C is fun')
-console.log('7-multi_c.js Missing number of occurrences')
-console.log('7-multi_c.js -3')
+const arg = process.argv[2]; const x = parseInt(arg);if (isNaN(x)) {console.log('Missing number of occurrences');} else {for (let i = 0; i < x; i++) {console.log('C is fun');}}
